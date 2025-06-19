e1202752e614f088f0fc7d4119537b95579f4156
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-06-19 11:53:48 +0200

    main page style

--- asset/script.js
+++ asset/script.js
@@ -16,7 +16,8 @@ function showproducts(products){
         const div = document.createElement("div")
         div.className= "card"
         div.innerHTML= `
-        <a  href="detailsProduit.html?id=${product.id} class="link"><img src=${product.thumbnail} alt="product"><br><br><h3>${product.title}</h3><br>Price:<strong>${product.price}</strong><br><em>Note: ${product.rating}</em><br><em>Catégorie: ${product.category}</em></a>`
+        <a href="detailsProduit.html?id=${product.id}" class="link">
+        <img src=${product.thumbnail} alt="product"><br><br><h3>${product.title}</h3><br>Price:<strong>${product.price}</strong><br><em>Note: ${product.rating}</em><br><em>Catégorie: ${product.category}</em></a>`
          contenaire.appendChild(div)
         
     });
